077b868655c4634d99f08b86c2c6afbe767a3d4b
booldook <booldook@gmail.com>
2021-08-10 15:39:37 +0900

    다시 설명 끝!

--- js/01.slide.js
+++ js/01.slide.js
@@ -4,22 +4,57 @@ var $slide = $wrapper.find('.slide-wrap');
 var $pager = $wrapper.find('.pager');
 var $btPrev = $wrapper.find('.bt-prev');
 var $btNext = $wrapper.find('.bt-next');
-var interval, idx = 0, gap = 2000, speed = 500, last = $slide.find('.slide').length - 1;
+var interval;			// setInterval을 넣어놓을 변수
+var idx = 0;			// animation이 움직일 값의 인자 0, -100%, -200% ...
+var gap = 2000;		// setInterval의 간격
+var speed = 500;	// animation speed
+var last = $slide.find('.slide').length - 1;		// $('.slide')의 마지막 index
 
 /************** user function *************/
-function ani(slideEl, pagerEl, speed, idx) {
-	
+function ani() {
+	$slide.stop().animate({ 'left': -idx * 100 + '%' }, speed);
+	$pager.removeClass('active');
+	$pager.eq(idx === last ? 0 : idx).addClass('active');
 }
 
 /************** event callback ************/
+function onEnter() {
+	clearInterval(interval);
+}
 
+function onLeave() {
+	interval = setInterval(onNext, gap);
+}
+
+function onPager() {
+	idx = $(this).index();
+	ani();
+}
+
+function onPrev() {
+	if(idx === 0) {
+		$slide.css('left', -last * 100 + '%');
+		idx = last;
+	}
+	idx--;
+	ani();
+}
+
+function onNext() {
+	if(idx === last) {
+		$slide.css('left', 0);
+		idx = 0;
+	}
+	idx++;
+	ani();
+}
 
 /*************** event init ***************/
-interval = setInterval(onInterval, gap);
 $wrapper.mouseenter(onEnter).mouseleave(onLeave);
-$pager.click(onPagerClick);
+$pager.click(onPager);
 $btPrev.click(onPrev);
 $btNext.click(onNext);
 
 /*************** start init ***************/
+interval = setInterval(onNext, gap);
 

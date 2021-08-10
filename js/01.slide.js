a3847f2d701b49fd97bee6df3425e8b6c4894fff
booldook <booldook@gmail.com>
2021-08-10 14:52:53 +0900

    01.slide 완성

--- js/01.slide.js
+++ js/01.slide.js
@@ -2,28 +2,18 @@
 var $wrapper = $('.wrapper1');
 var $slide = $wrapper.find('.slide-wrap');
 var $pager = $wrapper.find('.pager');
-var interval, n = 1, speed = 2000, aniSpeed = 500, cnt = $slide.find('.slide').length;
+var $btPrev = $wrapper.find('.bt-prev');
+var $btNext = $wrapper.find('.bt-next');
+var interval, n = 0, speed = 2000, aniSpeed = 500, cnt = $slide.find('.slide').length;
 
 /************** user function *************/
-function chgPager(el, idx) {
-	$(el).removeClass('active');
-	$(el).eq(idx).addClass('active');
+function ani(slideEl, pagerEl, speed, idx) {
+	$(pagerEl).removeClass('active');
+	$(pagerEl).eq(idx === cnt - 1 ? 0 : idx).addClass('active');
+	$(slideEl).stop().animate({'left': -idx * 100+'%'}, speed);
 }
 
 /************** event callback ************/
-function onAni() {
-	$slide.stop().animate({'left': -n * 100+'%'}, aniSpeed, function() {
-		if(n === cnt - 1) {
-			$slide.css('left', 0);
-			chgPager($pager, 0);
-			n = 1;
-		}
-		else {
-			chgPager($pager, n++);
-		}
-	});
-}
-
 function onEnter() {
 	clearInterval(interval);
 }
@@ -32,15 +22,42 @@ function onLeave() {
 	interval = setInterval(onAni, speed);
 }
 
+function onAni() {
+	if(n === cnt - 1) {
+		$slide.css('left', 0);
+		n = 1;
+	}
+	else n++;
+	ani($slide, $pager, aniSpeed, n);
+}
+
 function onPagerClick() {
 	n = $(this).index();
-	onAni();
+	ani($slide, $pager, aniSpeed, n);
+}
+
+function onPrev() {
+	if(n === 0) {
+		$slide.css('left', -(cnt - 1)*100 +'%');
+	}
+	n = (n === 0) ? cnt - 2 : n - 1;
+	ani($slide, $pager, aniSpeed, n);
+}
+
+function onNext() {
+	if(n === cnt - 1) {
+		$slide.css('left', 0);
+	}
+	n = (n === cnt - 1) ? 1 : n + 1;
+	ani($slide, $pager, aniSpeed, n);
 }
 
 /*************** event init ***************/
 interval = setInterval(onAni, speed);
 $wrapper.mouseenter(onEnter).mouseleave(onLeave);
 $pager.click(onPagerClick);
+$btPrev.click(onPrev);
+$btNext.click(onNext);
 
 /*************** start init ***************/
 

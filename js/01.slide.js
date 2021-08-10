c65ef50e407409198f8a1abf2fe17a24f63f0a99
booldook <booldook@gmail.com>
2021-08-10 13:20:20 +0900

    bt-angle 디자인 완료

--- js/01.slide.js
+++ js/01.slide.js
@@ -1,19 +1,46 @@
 /*************** global init **************/
-var $slide = $('.wrapper1 .slide-wrap');
-var interval, n = 1, cnt = $slide.find('.slide').length;
+var $wrapper = $('.wrapper1');
+var $slide = $wrapper.find('.slide-wrap');
+var $pager = $wrapper.find('.pager');
+var interval, n = 1, speed = 2000, aniSpeed = 500, cnt = $slide.find('.slide').length;
 
 /************** user function *************/
-
+function chgPager(el, idx) {
+	$(el).removeClass('active');
+	$(el).eq(idx).addClass('active');
+}
 
 /************** event callback ************/
-function onSet() {
-	$slide.stop().animate({'left': -(n * 100)+'%'}, 1000, function() {
-		++n
+function onAni() {
+	$slide.stop().animate({'left': -n * 100+'%'}, aniSpeed, function() {
+		if(n === cnt - 1) {
+			$slide.css('left', 0);
+			chgPager($pager, 0);
+			n = 1;
+		}
+		else {
+			chgPager($pager, n++);
+		}
 	});
 }
 
+function onEnter() {
+	clearInterval(interval);
+}
+
+function onLeave() {
+	interval = setInterval(onAni, speed);
+}
+
+function onPagerClick() {
+	n = $(this).index();
+	onAni();
+}
+
 /*************** event init ***************/
-interval = setInterval(onSet, 3000);
+interval = setInterval(onAni, speed);
+$wrapper.mouseenter(onEnter).mouseleave(onLeave);
+$pager.click(onPagerClick);
 
 /*************** start init ***************/
 

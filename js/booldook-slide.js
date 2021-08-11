ad6126bc9edd8b7ba5627322836f2366c145a7f7
booldook <booldook@gmail.com>
2021-08-11 14:31:31 +0900

    slide method 구현중

--- js/booldook-slide.js
+++ js/booldook-slide.js
@@ -1,4 +1,5 @@
 function Slide(_parent, _opt) {
+	var interval;
 	var opt = _opt || {};
 	this.wrapper = $(_parent);
 	this.wrap = this.wrapper.find('.slide-wrap');
@@ -11,25 +12,77 @@ function Slide(_parent, _opt) {
 		this.effect = 'horizontal';
 	}
 	this.effect += 'Type';
-	this.init();
-}
-
-Slide.prototype.init = function() {
 	this.wrap.addClass(this.effect);
+	
+	/******* init *******/
 	if(this.effect === 'fadeType') {
 		this.slide.eq(0).clone().appendTo(this.wrap).removeClass('slide'); // Dummy생성(높이생성)
+		fadeInit();
 	}
 	if(this.effect === 'horizontalType') {
 		this.slide.eq(0).clone().appendTo(this.wrap);
 		this.slide = this.wrap.find('.slide');
 		this.wrap.css('width', this.slide.length * 100 + '%');
 		this.slide.css('width', 100/this.slide.length.toFixed(4) + '%');
+		horiInit();
 	}
 	if(this.effect === 'verticalType') {
 		this.slide.eq(0).clone().appendTo(this.wrapper).removeClass('slide');
+		this.slide.eq(0).clone().appendTo(this.wrap);
 		this.slide = this.wrap.find('.slide');
+		vertInit();
+	}
+	
+	/******* horiInit *******/
+	function horiInit() {
+		var idx = 0, last = this.slide.length - 1;
+
+		this.wrap.mouseenter(onEnter).mouseleave(onLeave);
+		interval = setInterval(onNext, this.autoPlaySpeed);
+	}
+
+	/******* fadeInit *******/
+	function fadeInit() {
+		var idx = 0, last = this.slide.length - 1;
+
+		interval = setInterval(onNext, this.autoPlaySpeed);
+	}
+
+	/******* vertInit *******/
+	function vertInit() {
+		var idx = 0, last = this.slide.length - 1;
+
+		interval = setInterval(onNext, this.autoPlaySpeed);
+	}
+
+	/******* ani *******/
+	function ani() {
+
+	}
+
+	/******* onEnter *******/
+	function onEnter() {
+		clearInterval(interval);
+	}
+
+	/******* onLeave *******/
+	function onLeave() {
+		interval = setInterval(onNext, this.autoPlaySpeed);
 	}
 }
+
+Slide.prototype.onNext = function() {
+
+}
+
+Slide.prototype.onPrev = function() {
+
+}
+
+Slide.prototype.onPager = function() {
+
+}
+
 
 
 

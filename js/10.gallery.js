46c3291541777985c1ca56984ed1b29af4c425f6
booldook <booldook@gmail.com>
2021-08-12 12:51:15 +0900

    10.gallery-dataset

--- js/10.gallery.js
+++ js/10.gallery.js
@@ -5,10 +5,21 @@
 
 
 /************** event callback ************/
+function onClick() {
+	console.log( this.dataset['img'] );
+	console.log( $(this).data('img') );
+	$('.img-wrap img').attr('src', $(this).data('img'));
+	$('.img-wrap .name').html($(this).data('name'));
 
+	this.dataset['flag'] = 'ABCD';
+	$(this).data('flag2', 'ABCDE');
+
+	console.log( this.dataset['flag'] );
+	console.log( $(this).data('flag2') );
+}
 
 /*************** event init ***************/
-
+$('.thumb img').click(onClick);
 
 /*************** start init ***************/
 

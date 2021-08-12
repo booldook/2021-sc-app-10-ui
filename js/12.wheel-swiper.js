3fcf8602d486d4a92e81db471aa7625ce5a9f707
booldook <booldook@gmail.com>
2021-08-12 15:39:00 +0900

    완성

---
+++ js/12.wheel-swiper.js
@@ -0,0 +1,23 @@
+/*************** global init **************/
+
+
+/************** user function *************/
+
+
+/************** event callback ************/
+
+
+/*************** event init ***************/
+
+
+/*************** start init ***************/
+var swiper = new Swiper(".swiper-container", {
+	direction: "vertical",
+	slidesPerView: 1,
+	mousewheel: true,
+	speed: 800,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
